3a2805d7f2eb562d2fd7209f82b1bc746e05b94d
kenB <maiphuongbrt9a1@gmail.com>
2022-07-12 21:40:38 +0700

    first commit

---
+++ doctorStrange.h
@@ -0,0 +1,27 @@
+//The library here is concretely set, students are not allowed to include any other libraries.
+#ifndef doctorStrange_h
+#define doctorStrange_h
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+///STUDENT'S ANSWER BEGINS HERE
+///STUDENTS CAN ADD HELPER FUNCTIONS
+
+int handleEvents(string & HP, string & LV, string & EXP, string & TS, string & events) {
+    ///Students have to complete this function and DO NOT modify any parameters in this function.
+    int result = 0;
+    cout << HP << " " << LV << " " << EXP << " " << TS << endl;
+    cout << events << endl;
+    return result;
+}
+
+///END OF STUDENT'S ANSWER
+
+#endif /* studyInPink_h */
